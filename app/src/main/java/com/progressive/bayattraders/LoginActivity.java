653ac742ffe9009eea7bc2b21f1aae86bbd9d6eb
aliyousafphularwan <victamoflove@gmail.com>
2021-05-31 12:10:53 +0500

    login page

--- app/src/main/java/com/progressive/bayattraders/LoginActivity.java
+++ app/src/main/java/com/progressive/bayattraders/LoginActivity.java
@@ -2,9 +2,26 @@ package com.progressive.bayattraders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.progressive.bayattraders.helpers.Constants;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +29,54 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().hide();
+
+        username = findViewById(R.id.login_username);
+        password = findViewById(R.id.login_password);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user = username.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+
+                getLogin();
+
+            }
+        });
+
+    }
+
+    private void getLogin() {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.login, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    JSONObject jo = new JSONObject(response);
+                    String status = jo.getString("stautus");
+                    if(status.equals("1")){
+                        startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                        finish();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(LoginActivity.this, "", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(LoginActivity.this, "Error: Check Internet", Toast.LENGTH_SHORT).show();
+
+            }
+        }){
+
+        };
+
     }
 }
