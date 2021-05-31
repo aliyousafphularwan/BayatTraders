7e745df93d112b1f6b3a5fb29328e9e3f6093221
aliyousafphularwan <victamoflove@gmail.com>
2021-05-31 11:58:44 +0500

    updated

--- app/src/main/java/com/progressive/bayattraders/helpers/Constants.java
+++ app/src/main/java/com/progressive/bayattraders/helpers/Constants.java
@@ -1,4 +1,11 @@
 package com.progressive.bayattraders.helpers;
 
 public class Constants {
+
+    private static final String URL = "http://soft.bayattraders.co.uk/apis/";
+    public static final String login = "login.php";
+    public static final String signup = "creat_user.php";
+    public static final String user = "profile.php";
+    public static final String ex_rate = "ex_rate.php";
+
 }
