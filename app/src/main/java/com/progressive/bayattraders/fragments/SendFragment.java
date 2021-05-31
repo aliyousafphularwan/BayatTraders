fa152812a1a139018bea2a192243aad800c250a8
aliyousafphularwan <victamoflove@gmail.com>
2021-05-31 15:43:34 +0500

    updates

--- app/src/main/java/com/progressive/bayattraders/fragments/SendFragment.java
+++ app/src/main/java/com/progressive/bayattraders/fragments/SendFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.progressive.bayattraders.R;
@@ -18,6 +22,8 @@ public class SendFragment extends Fragment implements AdapterView.OnItemSelected
 
     Spinner country;
     ImageView flag;
+    TextView ttlsndngamnt;
+    EditText send_amount;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,11 +31,32 @@ public class SendFragment extends Fragment implements AdapterView.OnItemSelected
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_send, container, false);
 
+        ttlsndngamnt = v.findViewById(R.id.ttlsndngamnt);
+
         country = v.findViewById(R.id.rcv_country);
         country.setOnItemSelectedListener(this);
 
         flag = v.findViewById(R.id.country_flag);
         flag.setVisibility(View.INVISIBLE);
+
+        send_amount = v.findViewById(R.id.send_amount);
+        send_amount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ttlsndngamnt.setText(send_amount.getText().toString() + " GBP");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         return v;
     }
 
